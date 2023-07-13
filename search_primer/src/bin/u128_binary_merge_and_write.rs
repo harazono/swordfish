5bd95786437858d23125be679cc0e84d220fce95
zono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2023-07-13 14:09:04 +0900

    WIP

---
+++ search_primer/src/bin/u128_binary_merge_and_write.rs
@@ -0,0 +1,61 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Read, Write, BufRead};
+use getopts::Options;
+use std::env;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let mut opts = Options::new();
+    opts.optopt("i", "input", "set input file name", "NAME");
+    opts.optopt("o", "output", "set output directory", "DIR");
+    opts.optopt("p", "prefix", "set output file prefix", "PREFIX");
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!("{}", f.to_string()),
+    };
+    let input_file_name = matches.opt_str("i").unwrap();
+    let output_dir = matches.opt_str("o").unwrap();
+    let output_prefix = matches.opt_str("p").unwrap();
+
+    let mut set = HashSet::new();
+
+    // Read all files
+    let file = File::open(input_file_name)?;
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let path = line?;
+        let file = File::open(path)?;
+        let mut reader = BufReader::new(file);
+
+        loop {
+            let mut buffer = [0; 16]; // u128 is 16 bytes
+            match reader.read_exact(&mut buffer) {
+                Ok(_) => {
+                    let value = u128::from_le_bytes(buffer);
+                    set.insert(value);
+                }
+                Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => break,
+                Err(e) => return Err(e),
+            }
+        }
+    }
+
+    // Write to output files
+    let values_per_file = (set.len() + 4999) / 5000; // ceil division
+    let mut file_index = 0;
+    let mut values_in_file = 0;
+    let mut writer = BufWriter::new(File::create(format!("{}/{}{}.bin", output_dir, output_prefix, file_index))?);
+
+    for value in set {
+        if values_in_file == values_per_file {
+            file_index += 1;
+            values_in_file = 0;
+            writer = BufWriter::new(File::create(format!("{}/{}{}.bin", output_dir, output_prefix, file_index))?);
+        }
+        writer.write_all(&value.to_le_bytes())?;
+        values_in_file += 1;
+    }
+
+    Ok(())
+}
