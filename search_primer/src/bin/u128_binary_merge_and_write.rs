750ce20a2f334f27fc3ef163a96f15a20e290ec3
zono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2023-07-13 15:43:35 +0900

    WIP

--- search_primer/src/bin/u128_binary_merge_and_write.rs
+++ search_primer/src/bin/u128_binary_merge_and_write.rs
@@ -5,7 +5,7 @@ use getopts::Options;
 use std::env;
 
 fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} -i INPUT -o OUTPUT -p PREFIX", program);
+    let brief = format!("Usage: {} -i INPUT -o OUTPUT -p PREFIX [-n NUMFILES | -s FILESIZE]", program);
     print!("{}", opts.usage(&brief));
 }
 
@@ -17,6 +17,8 @@ fn main() -> std::io::Result<()> {
     opts.optopt("i", "input", "set input file name", "NAME");
     opts.optopt("o", "output", "set output directory", "DIR");
     opts.optopt("p", "prefix", "set output file prefix", "PREFIX");
+    opts.optopt("n", "numfiles", "set number of output files", "NUM");
+    opts.optopt("s", "filesize", "set max size of each output file", "SIZE");
     opts.optflag("h", "help", "print this help menu");
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
@@ -30,16 +32,25 @@ fn main() -> std::io::Result<()> {
         print_usage(&program, opts);
         return Ok(());
     }
-	
     let input_file_name = matches.opt_str("i").unwrap();
-    let output_dir = matches.opt_str("o").unwrap();
-    let output_prefix = matches.opt_str("p").unwrap();
+    let output_dir      = matches.opt_str("o").unwrap();
+    let output_prefix   = matches.opt_str("p").unwrap();
+    let num_files       = matches.opt_str("n").map(|n| n.parse::<usize>().unwrap());
+    let file_size       = matches.opt_str("s").map(|s| s.parse::<usize>().unwrap());
 
-    let mut set = HashSet::new();
+    if num_files.is_some() && file_size.is_some() {
+        eprintln!("Error: Cannot specify both number of files and file size");
+        print_usage(&program, opts);
+        return Ok(());
+    }
 
+    
     // Read all files
     let file = File::open(input_file_name)?;
     let reader = BufReader::new(file);
+    let mut intermediate_files = Vec::new();
+    let mut set: HashSet<u128> = HashSet::new();
+
     for (index, line) in reader.lines().enumerate() {
         let path = line?;
         let file = File::open(&path)?;
@@ -57,10 +68,56 @@ fn main() -> std::io::Result<()> {
             }
         }
         eprintln!("Finished reading file {}: {}", index + 1, path);
+
+        // If we've read 20 files, write the set to an intermediate file
+        if (index + 1) % 20 == 0 {
+            let intermediate_file_name = format!("intermediate_{}.bin", (index + 1) / 20);
+            let mut writer = BufWriter::new(File::create(&intermediate_file_name)?);
+            for value in &set {
+                writer.write_all(&value.to_le_bytes())?;
+            }
+            intermediate_files.push(intermediate_file_name);
+            set.clear(); // Clear the set for the next batch of files
+        }
+    }
+
+    // Don't forget the last batch if it's less than 20 files
+    if !set.is_empty() {
+        let intermediate_file_name = format!("intermediate_{}.bin", intermediate_files.len() + 1);
+        let mut writer = BufWriter::new(File::create(&intermediate_file_name)?);
+        for value in &set {
+            writer.write_all(&value.to_le_bytes())?;
+        }
+        intermediate_files.push(intermediate_file_name);
+    }
+
+    // Now merge the intermediate files
+    let mut set = HashSet::new();
+    for (index, file_name) in intermediate_files.iter().enumerate() {
+        let file = File::open(file_name)?;
+        let mut reader = BufReader::new(file);
+
+        loop {
+            let mut buffer = [0; 16]; // u128 is 16 bytes
+            match reader.read_exact(&mut buffer) {
+                Ok(_) => {
+                    let value = u128::from_le_bytes(buffer);
+                    set.insert(value);
+                }
+                Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => break,
+                Err(e) => return Err(e),
+            }
+        }
+        eprintln!("Finished merging file {}: {}", index + 1, file_name);
     }
 
     // Write to output files
-    let values_per_file = (set.len() + 4999) / 5000; // ceil division
+    let values_per_file = match (num_files, file_size) {
+        (Some(n), None) => (set.len() + n - 1) / n, // ceil division
+        (None, Some(s)) => s / 16, // each value is 16 bytes
+        (None, None) => (set.len() + 4999) / 5000, // default to 5000 files
+        _ => unreachable!(), // This should never happen
+    };
     let mut file_index = 0;
     let mut values_in_file = 0;
     let mut writer = BufWriter::new(File::create(format!("{}/{}{}.bin", output_dir, output_prefix, file_index))?);
