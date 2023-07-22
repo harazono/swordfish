88bf8456711d541158aa1478e651e1d07f33c4a3
zono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2023-07-23 03:08:18 +0900

    WIP

--- search_primer/src/bin/primer3_caller.rs
+++ search_primer/src/bin/primer3_caller.rs
@@ -153,16 +153,17 @@ fn main(){
             thread::spawn(move|| {
                 eprintln!("Thread[{}]: start  calling primer3_core", i);
                 let total_bunches = chunks_of_input[i].len();
+                let mut primer3_results = String::new();
                 for (j, bunch) in chunks_of_input[i].iter().enumerate(){
                     let start_time = std::time::Instant::now(); // Start timing here
                     let joined_bunch = bunch.join("\n");
-                    let primer3_results: String = execute_primer3((joined_bunch).to_string());
-                    arc_final_result.lock().unwrap().push(primer3_results);
+                    primer3_results += &execute_primer3((joined_bunch).to_string());
                     // Calculate progress as a percentage
                     let progress = ((j + 1) as f64 / total_bunches as f64) * 100.0;
                     let duration = start_time.elapsed(); // Get the time elapsed since start_time
-                    eprintln!("Thread{} [{}%]: processed bunch {} of {} in {:.2?} seconds", i, progress, j + 1, total_bunches, duration);
+                    eprintln!("Thread[{}] [{:.2?}%]: processed bunch {} of {} in {:.2?}", i, progress, j + 1, total_bunches, duration);
                 }
+                arc_final_result.lock().unwrap().push(primer3_results);
                 eprintln!("Thread[{}]: finish calling primer3_core", i);
         })
         );
