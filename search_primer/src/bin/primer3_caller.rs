010382bb069e0b53ec2c3c2eadb4f840f8c10625
zono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2023-07-23 03:10:19 +0900

    WIP

--- search_primer/src/bin/primer3_caller.rs
+++ search_primer/src/bin/primer3_caller.rs
@@ -133,7 +133,7 @@ fn main(){
 
     eprintln!("start formatting string");
     let primer3_fmt_string: Vec<String> = primer3_core_input_sequence(&candidates, &library_file_name);
-    let bunch_of_50000_fmt_string: Vec<Vec<String>> = primer3_fmt_string.chunks(500).map(|chunk| chunk.to_vec()).collect();
+    let bunch_of_50000_fmt_string: Vec<Vec<String>> = primer3_fmt_string.chunks(1).map(|chunk| chunk.to_vec()).collect();
     let mut chunks_of_input: Vec<Vec<Vec<String>>> = Vec::new();
     for i in 0..thread_number{
         chunks_of_input.push(Vec::new());
