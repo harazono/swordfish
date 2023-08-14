a1b515ef652479b2ec50d05c067d1c75ae97a524
zono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2023-08-14 15:35:11 +0900

    WIP

--- util/blast_result_analysis.py
+++ util/blast_result_analysis.py
@@ -117,7 +117,7 @@ def main():
                 distance = each_record_Obj.qlen - each_record_Obj.qend
             if each_record_Obj.qseqid.endswith("R"):
                 distance = each_record_Obj.qstart
-            print(each_record_Obj, distance, file = sys.stderr)
+            print(distance, args.offset, distance > args.offset, each_record_Obj, file = sys.stderr)
 
             if distance > args.offset:
                 continue#救済
