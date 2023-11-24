97fbd07f8e900b782b044a03fc81929c226fa2a3
zono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2023-11-24 22:12:58 +0900

    コマンドライン引数や変数名を整理。

--- search_primer/src/bin/extract_PCR_target_region.rs
+++ search_primer/src/bin/extract_PCR_target_region.rs
@@ -4,36 +4,38 @@ extern crate rdxsort;
 use crate::bio::io::fasta::FastaRead;
 use bio::io::fasta::Reader as faReader;
 use bio::io::fasta::Record as faRecord;
+use core::any::Any;
 use getopts::Options;
 use search_primer::counting_bloomfilter_util::aggregate_length_between_lr_tuple;
 use search_primer::sequence_encoder_util::DnaSequence;
+// use std::any::Any;
+use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::{BufRead, Write};
 use std::sync::Arc;
 use std::thread;
-use std::{env, process};
 
 fn print_usage(program: &str, opts: &Options) {
     let brief = format!("Usage: {} FILE [options]", program);
     print!("{}", opts.usage(&brief));
-    process::exit(0);
 }
-
-fn main() {
+fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
 
     let mut opts = Options::new();
-    opts.optopt("i", "input", "set input file name (TSV)", "NAME");
     opts.optopt("o", "output", "set output file name", "NAME");
+    opts.optopt("r", "read", "set NGS read file name", "READ");
+    opts.optopt("t", "tsv", "set TSV file name", "TSV");
+
     opts.optopt(
-        "t",
+        "T",
         "thread",
-        "number of threads to use for radix sort. default value is 8.",
+        "number of threads to use for processing. Default is 8.",
         "THREAD",
     );
-    opts.optopt("l", "length", "maximam size of target region", "LENGTH");
+    opts.optopt("l", "length", "maximum size of target region", "LENGTH");
 
     opts.optflag("h", "help", "print this help menu");
 
@@ -43,52 +45,48 @@ fn main() {
             panic!("{}", f.to_string())
         }
     };
+
     if matches.opt_present("h") {
         print_usage(&program, &opts);
-        return;
+        return Ok(());
     }
 
-    let primer_filename: String = if matches.opt_present("i") {
-        matches.opt_str("i").unwrap()
-    } else {
+    let output_file = matches
+        .opt_str("o")
+        .unwrap_or_else(|| "output.out".to_string());
+    let ngsread_file = matches.opt_str("r").ok_or_else(|| {
         print_usage(&program, &opts);
-        return;
-    };
-
-    let ngsread_input_file = if !matches.free.is_empty() {
-        matches.free[0].clone()
-    } else {
+        "NGS read file name is required".to_string()
+    })?;
+    let tsv_file = matches.opt_str("t").ok_or_else(|| {
         print_usage(&program, &opts);
-        return;
-    };
+        "TSV file name is required".to_string()
+    })?;
+    let threads: usize = matches
+        .opt_str("T")
+        .unwrap_or("8".to_string())
+        .parse()
+        .unwrap();
+    let max_offtarget_region_length: usize = matches
+        .opt_str("l")
+        .unwrap_or("200".to_string())
+        .parse()
+        .unwrap();
 
-    let threads: usize = if matches.opt_present("t") {
-        matches.opt_str("t").unwrap().parse::<usize>().unwrap()
-    } else {
-        8
-    };
-
-    let max_offtarget_region_length: usize = if matches.opt_present("l") {
-        matches.opt_str("l").unwrap().parse::<usize>().unwrap()
-    } else {
-        200
-    };
-
-    let output_file = if matches.opt_present("o") {
-        matches.opt_str("o").unwrap()
-    } else {
-        "region.out".to_string()
-    };
-
-    eprintln!("Input  file name: {:?}", ngsread_input_file);
     eprintln!("Output file name: {:?}", output_file);
+    eprintln!("NGS read file name: {:?}", ngsread_file);
+    eprintln!("TSV file name: {:?}", tsv_file);
     eprintln!("Number of threads: {:?}", threads);
+    eprintln!(
+        "Maximum off-target region length: {:?}",
+        max_offtarget_region_length
+    );
 
     let mut primer_tuple: Vec<(Vec<u8>, DnaSequence, DnaSequence)> = Vec::new();
 
-    let f: File = File::open(&primer_filename).unwrap();
-    let reader = BufReader::new(f);
-    let mut lines = reader.lines();
+    let f: File = File::open(&tsv_file).unwrap();
+    let reader: BufReader<File> = BufReader::new(f);
+    let mut lines: std::io::Lines<BufReader<File>> = reader.lines();
     lines.next(); // ヘッダー行をスキップ
 
     lines.for_each(|line| {
@@ -99,24 +97,26 @@ fn main() {
             let primer_id: &[u8] = columns.get(0).unwrap_or(&"").as_bytes();
             let left_primer_seq: &str = columns.get(1).unwrap_or(&"");
             let right_primer_seq: &str = columns.get(2).unwrap_or(&"");
-            let left_primer: DnaSequence = DnaSequence::new(&left_primer_seq.into());
-            let right_primer: DnaSequence = DnaSequence::new(&right_primer_seq.into());
-            let left_primer_revcomp: DnaSequence = left_primer.reverse_complement();
-            let right_primer_revcomp: DnaSequence = right_primer.reverse_complement();
-            let id_1: Vec<u8> = [&primer_id, &b"LeftPrimerForward_LeftPrimerRevcomp"[..]].concat();
-            let id_2: Vec<u8> = [&primer_id, &b"LeftPrimerForward_RightPrimerRevcomp"[..]].concat();
-            let id_3: Vec<u8> = [&primer_id, &b"LeftPrimerRevcomp_RightPrimerRevcomp"[..]].concat();
-            let id_4: Vec<u8> =
-                [&primer_id, &b"RightPrimerRevcomp_RightPrimerForward"[..]].concat();
+            /*
+            入力TSVのLプライマーはリードと同じstrand、Rはrevcompとする。
+            R側のプライマーの読み込み時にrevcompに変換する。
+            リードの座標を基準に5to3か3to5か決める。
+            5'---> 3'
+             */
+            let left_primer_5to3: DnaSequence = DnaSequence::new(&left_primer_seq.into());
+            let right_primer_3to5: DnaSequence =
+                DnaSequence::new(&right_primer_seq.into()).reverse_complement();
+            let left_primer_3to5: DnaSequence = left_primer_5to3.reverse_complement();
+            let right_primer_5to3: DnaSequence = right_primer_3to5.reverse_complement();
+            let id_1: Vec<u8> = [&primer_id, &b"L-L"[..]].concat();
+            let id_2: Vec<u8> = [&primer_id, &b"L-R"[..]].concat();
+            let id_3: Vec<u8> = [&primer_id, &b"R-L"[..]].concat();
+            let id_4: Vec<u8> = [&primer_id, &b"R-R"[..]].concat();
 
-            primer_tuple.push((id_1, left_primer.clone(), left_primer_revcomp.clone()));
-            primer_tuple.push((id_2, left_primer.clone(), right_primer_revcomp.clone()));
-            primer_tuple.push((
-                id_3,
-                left_primer_revcomp.clone(),
-                right_primer_revcomp.clone(),
-            ));
-            primer_tuple.push((id_4, right_primer_revcomp.clone(), right_primer.clone()));
+            primer_tuple.push((id_1, left_primer_5to3.clone(), left_primer_3to5.clone()));
+            primer_tuple.push((id_2, left_primer_5to3.clone(), right_primer_3to5.clone()));
+            primer_tuple.push((id_3, right_primer_5to3.clone(), left_primer_3to5.clone()));
+            primer_tuple.push((id_4, right_primer_5to3.clone(), right_primer_3to5.clone()));
         }
     });
 
@@ -133,32 +133,33 @@ fn main() {
         );
     }
     */
-    let ngsread_file = File::open(&ngsread_input_file).expect("Error during opening the file");
-    eprintln!("loading {:?}", &ngsread_input_file);
-    let mut reader = faReader::new(ngsread_file);
-    let mut record = faRecord::new();
+    let ngsread_file_obj: File = File::open(&ngsread_file).expect("Error during opening the file");
+    eprintln!("loading {:?}", &ngsread_file);
+    let mut reader: faReader<BufReader<File>> = faReader::new(ngsread_file_obj);
+    let mut record: faRecord = faRecord::new();
     let mut sequences: Vec<DnaSequence> = Vec::new();
-    eprintln!("loading {:?} done", &ngsread_input_file);
+    eprintln!("loading {:?} done", &ngsread_file);
     'each_read: loop {
         reader.read(&mut record).unwrap();
         if record.is_empty() {
             break 'each_read;
         }
         let sequence_as_vec: Vec<u8> = record.seq().to_vec();
-        let current_sequence = DnaSequence::new(&sequence_as_vec);
+        let current_sequence: DnaSequence = DnaSequence::new(&sequence_as_vec);
         sequences.push(current_sequence);
     }
     let chunk_size: usize = sequences.len() / (threads - 1);
-    let sequences_ref = Arc::new(sequences);
-    let primer_tuple_ref = Arc::new(primer_tuple);
+    let sequences_ref: Arc<Vec<DnaSequence>> = Arc::new(sequences);
+    let primer_tuple_ref: Arc<Vec<(Vec<u8>, DnaSequence, DnaSequence)>> = Arc::new(primer_tuple);
     //let mut cbf_oyadama: Vec<u32> = vec![0;BLOOMFILTER_TABLE_SIZE];
 
     //let mut product_size_hashmap = HashMap::<u32, usize>::new();
     thread::scope(|scope| {
-        let mut children_1 = Vec::new();
+        let mut children_1: Vec<thread::ScopedJoinHandle<'_, Vec<u8>>> = Vec::new();
         for i in 1..threads {
-            let sequences_ref = Arc::clone(&sequences_ref);
-            let primer_tuple_ref = Arc::clone(&primer_tuple_ref);
+            let sequences_ref: Arc<Vec<DnaSequence>> = Arc::clone(&sequences_ref);
+            let primer_tuple_ref: Arc<Vec<(Vec<u8>, DnaSequence, DnaSequence)>> =
+                Arc::clone(&primer_tuple_ref);
             children_1.push(scope.spawn(move || {
                 let start_idx: usize = (i - 1) * chunk_size;
                 let end_idx: usize;
@@ -167,8 +168,10 @@ fn main() {
                 } else {
                     end_idx = sequences_ref.len() - 1;
                 }
-                let primer_tuple_ref_mine = (*primer_tuple_ref).clone();
-                let slice_sequences = Vec::from(sequences_ref[start_idx..end_idx].to_vec());
+                let primer_tuple_ref_mine: Vec<(Vec<u8>, DnaSequence, DnaSequence)> =
+                    (*primer_tuple_ref).clone();
+                let slice_sequences: Vec<DnaSequence> =
+                    Vec::from(sequences_ref[start_idx..end_idx].to_vec());
 
                 eprintln!(
                     "start calling aggregate_length_between_primer_tuple[{}], # of sequence: {}",
@@ -189,11 +192,12 @@ fn main() {
             }))
         }
         eprintln!("start  writing to output file: {:?}", &output_file);
-        let mut file = File::create(&output_file).unwrap();
+        let mut file: File = File::create(&output_file).unwrap();
         for child in children_1 {
-            let result = child.join(); // Resultを返さず、直接戻り値を取得
+            let result: Result<Vec<u8>, Box<dyn Any + Send>> = child.join(); // Resultを返さず、直接戻り値を取得
             file.write_all(&result.unwrap()).unwrap();
         }
         eprintln!("finish writing to output file: {:?}", &output_file);
     });
+    Ok(())
 }
