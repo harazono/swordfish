77fa2d76fc889412751d423a18b3fec2bb26fb25
zono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2024-04-09 11:33:06 +0900

    WIP

--- util/redesign_primers_with_probe.py
+++ util/redesign_primers_with_probe.py
@@ -26,8 +26,8 @@ def main():
         many_n = "N" * 20
         concatinated_sequence = f"{l}{many_n}{m}{many_n}{r}"
         # print(concatinated_sequence)
-        config_file_name = os.path.join(args.output_dir, group, ".config")
-        output_path = os.path.join(args.output_dir, group, ".primer3.out")
+        config_file_name = os.path.join(args.output_dir, group, "config")
+        output_path = os.path.join(args.output_dir, group, "primer3.out")
         run_primer3(
             concatinated_sequence,
             args.primer3_config_template_path,
@@ -64,10 +64,12 @@ def run_primer3(
     primer3_config_path,
     primer3_output_path,
 ):
-    if not os.path.exists("primer3_config"):
-        os.makedirs("primer3_config")
-    if not os.path.exists("primer3_results"):
-        os.makedirs("primer3_results")
+    if not os.path.exists(primer3_config_path):
+        os.makedirs(primer3_config_path)
+    if not os.path.exists(primer3_output_path):
+        os.makedirs(primer3_output_path)
+    if not os.path.exists(primer3_template_path):
+        os.makedirs(primer3_template_path)
 
     with open(primer3_template_path, "r") as template_file:
         config_content = template_file.read()
