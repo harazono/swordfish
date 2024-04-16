fbc0a9f9ad23853405213db8b8fd872878ff48de
zono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2024-04-16 22:10:17 +0900

    WIP

--- util/primer_alignment_results_parser.py
+++ util/primer_alignment_results_parser.py
@@ -10,13 +10,25 @@ def parse_blast_output(blast_output_file):
     with open(blast_output_file, 'r') as file:
         for line in file:
             parts = line.strip().split('\t')
-            query_id = parts[0]
-            sseqid   = parts[1]
-            sstart   = int(parts[7])
-            send     = int(parts[8])
+            sseqid = parts[1]
+            record = dict(
+                qseqid=parts[0],
+                sseqid=parts[1],
+                sacc=parts[2],
+                qlen=parts[3],
+                qstart=int(parts[4]),
+                qend=int(parts[5]),
+                slen=int(parts[6]),
+                sstart=int(parts[7]),
+                send=int(parts[8]),
+                qseq=parts[9],
+                sseq=parts[10],
+                length=int(parts[11]),
+                sstrand=parts[12],
+            )
             if sseqid not in hits: 
                 hits[sseqid] = []
-            hits[sseqid].append((query_id, sstart, send))
+            hits[sseqid].append(record)
     return hits
 
 def extract_amplicons(fasta_file, primer_hits, max_length):
@@ -38,17 +50,18 @@ def extract_amplicons(fasta_file, primer_hits, max_length):
         for v in itertools.combinations(hits, 2):
             hit_1, hit_2 = v
             sequence = str(record.seq).upper()
-            # hit_1が右向き（end - start > 0）で、hit_2が左向きの場合
-            if hit_1[2] - hit_1[1] > 0 and hit_2[1] - hit_2[2] > 0 and hit_2[2] - hit_1[1] < max_length:
-                # read.idとhit_1[1]とhit_2[2]の値を配列名とするfastaを生成してampliconsに追加。配列はsequenceの部分配列とする。
-                amplicon_name = f"{record.id}|{hit_1[0]}|{hit_2[0]}"
-                amplicon_sequence = sequence[hit_1[1]:hit_2[2]]
-                amplicons.append((amplicon_name, hit_1[1], hit_2[2], amplicon_sequence))
-            # hit_1が左向き（end - start < 0）で、hit_2が右向きの場合
-            elif hit_1[2] - hit_1[1] < 0 and hit_2[1] - hit_2[2] > 0 and hit_1[2] - hit_2[1] < max_length:
-                amplicon_name = f"{record.id}|{hit_1[0]}|{hit_2[0]}"
-                amplicon_sequence = sequence[hit_2[1]:hit_1[2]]
-                amplicons.append((amplicon_name, hit_2[1], hit_1[2], amplicon_sequence))
+            if hit_1.strand == hit_2.strand:
+                continue
+            if hit_1.send < hit_2.sstart:
+                amplicon_name = f"{record.id}|{hit_1.qseqid}{hit_1.strand}|{hit_2.qseqid}{hit_2.strand}"
+                assert hit_1.sstart < hit_2.send, "assertion failed"
+                amplicon_sequence = sequence[hit_1.sstart:hit_2.send]
+                amplicons.append((amplicon_name, amplicon_sequence))
+            if hit_2.send < hit_1.sstart:
+                amplicon_name = f"{record.id}|{hit_1.qseqid}{hit_1.strand}|{hit_2.qseqid}{hit_2.strand}"
+                assert hit_2.sstart < hit_1.send, "assertion failed"
+                amplicon_sequence = sequence[hit_2.sstart:hit_1.send]
+                amplicons.append((amplicon_name, amplicon_sequence))
     return amplicons
 
 def write_hits_to_file(hits, output_file):
@@ -59,8 +72,7 @@ def write_hits_to_file(hits, output_file):
         for sseqid, hit_list in hits.items():
             f.write(f"{sseqid}\n")
             for hit in hit_list:
-                qid, start, end = hit
-                f.write(f"\t{qid}\t{start}\t{end}\n")
+                f.write("\t".join([f"{str(k)}:{str(v)}" for k,v in hit]) + "\n")
 
 def main(args):
     primer_hits = parse_blast_output(args.blast_output)
@@ -68,8 +80,8 @@ def main(args):
     amplicons = extract_amplicons(args.fasta_file, primer_hits, args.max_length)
     with open(args.output_file + ".fa", 'w') as f:
         for amplicon_info in amplicons:
-            if len(amplicon_info[3]) != 0:
-                f.write(f">{amplicon_info[0]}\n{amplicon_info[3]}\n")
+            if len(amplicon_info[1]) != 0:
+                f.write(f">{amplicon_info[0]}\n{amplicon_info[1]}\n")
 
 
 if __name__ == "__main__":
